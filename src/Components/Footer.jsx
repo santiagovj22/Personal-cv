0fb6d9667ed9efca670c059f30f07fbab2a58fa5
pechuga22 <iefaz.santiagovalle@gmail.com>
2019-08-15 00:58:08 -0500

    actualizado

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,14 +4,8 @@ import React from 'react'
 const Footer=()=>{
     return(
         <div>
-            <footer className="page-footer font-small blue">
-
-               
-                <div className="footer-copyright text-center py-3 text-white" >© 2019 Copyright: By Santiago Valle
-                
-                </div>
-               
-
+            <footer className="page-footer font-small">
+                <div className="footer-copyright text-center py-3 text-white" > 2019 Copyright  By Santiago Valle ©</div>
             </footer>
         </div>
     );
