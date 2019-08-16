8593f01aac8cf76b0d87a7eb127c885707a129f7
pechuga22 <iefaz.santiagovalle@gmail.com>
2019-08-16 00:04:18 -0500

    actualizado

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,9 +14,9 @@ const Navbar=()=>{
           <div className="navbar-collapse collapse" id="navbarResponsive">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active"><a className="nav-link" href="#home">Home<span className="sr-only">(current)</span></a></li>
-              <li className="nav-item"><a className="nav-link" href="#projects">Projects</a></li>
-              <li className="nav-item"><a className="nav-link" href="#services">Services</a></li>
-              <li className="nav-item"><a className="nav-link" href="#skills">Skills</a></li>
+              <li className="nav-item"><a className="nav-link text-white" href="#projects">Projects</a></li>
+              <li className="nav-item"><a className="nav-link text-white" href="#services">Services</a></li>
+              <li className="nav-item"><a className="nav-link text-white" href="#skills">Skills</a></li>
             </ul>
           </div>
         </div>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Rigctus from '../Images/logoRigctus.png';
 import Tienda from '../Images/1.jpg';
 import Geek from '../Images/2.png';
+import Conf from '../Images/speaker1.jpg';
 
 const Projects=()=>{
     return(
@@ -10,30 +11,39 @@ const Projects=()=>{
           <div className="row">
             <div className="col-md-4 mb-5">
               <div className="h-70">
-                <img className="card-img-top" src={Rigctus} alt=""/>
+                <a href="a"><img className="card-img-top" src={Rigctus} alt=""/></a>
                   <div className="card-body">
-                    <h4 className="text-white">Rigctus<a href="a" className="btn btn-primary float-right">Visit</a></h4>
+                    <h4 className="text-white">Rigctus</h4>
                     <p className="card-text text-white">Would you like to water your crop and have all the information about it in one click? Rigctus is a mobile app that will help you in whatever you need</p>
                   </div>
                </div>
             </div>
             <div className="col-md-4 mb-5">
               <div className="h-70">
-                <img className="card-img-top" src={Tienda}alt=""/>
+                <a href="https://tienda-virtual-geek.firebaseapp.com/" rel="noopener noreferrer"><img className="card-img-top" src={Tienda}  alt=""/></a>
                 <div className="card-body">
-                  <h4 className="text-white">Furniture<a href="https://tienda-virtual-geek.firebaseapp.com/" rel="noopener noreferrer" className="btn btn-primary float-right">Visit</a></h4>
+                  <h4 className="text-white">Furniture</h4>
                   <p className="card-text text-white">Furniture gallery shop, everything you need for your home, rooms, dining rooms and alcoves at great price come now and find out the best promotions</p>
                 </div>
               </div>
             </div>
             <div className="col-md-4 mb-5">
               <div className="h-70">
-                <img className="card-img-top" src={Geek}alt="" height="350"/>
+              <a href="https://academiageek.netlify.com/" rel="noopener noreferrer"><img className="card-img-top" src={Geek}alt="" height="350"/></a>
                 <div className="card-body">
-                  <h4 className="text-white">Academia Geek <a href="https://academiageek.netlify.com/" rel="noopener noreferrer" className="btn btn-primary float-right">Visit</a></h4>
+                  <h4 className="text-white">Academia Geek </h4>
                   <p className="card-text text-white">Learn how to build web applications with our 8-week face-to-face course. You will be able to access the world of work and work with our allies.</p>
                 </div> 
               </div>
+            </div>
+            <div className="col-md-4 mb-5">
+              <div className="h-70">
+                <a href="https://geek-conf1.netlify.com/" rel="noopener noreferrer"><img className="card-img-top" src={Conf} alt=""/></a>
+                  <div className="card-body">
+                    <h4 className="text-white">Geek Conf</h4>
+                    <p className="card-text text-white">Geek conf performs its first Demo Day with the line cohort of Line 1: FrontEnd, DOnde expose a project with what we learned in the academic geek course</p>
+                  </div>
+               </div>
             </div>
 </div>
 
