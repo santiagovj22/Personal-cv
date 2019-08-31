7f39f56a287854b93d9eedf5b064f7106492f38c
pechuga22 <iefaz.santiagovalle@gmail.com>
2019-08-30 21:24:55 -0500

    actualizado

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,7 +8,7 @@ const About=()=>{
                         <div className="col-md-8 mb-5 text-white">
                             <h2>What Can I Do</h2>
                             <hr/>
-                            <p>I am a Software Developer with experience in front-end / Back-end areas, i am able to identify customer needs and create innovate alternatives for their projects and i have the ability to provide solutions in the area of software development</p>
+                            <p>I am a Software Developer with experience in front-end / Back-end areas, i am able to identify customer needs and create innovate alternatives for their projects and i have the ability to provide solutions in the area of software development </p>
                             <a className="btn btn-primary btn-lg" href="https://wa.me/573133461897?text=Me%20gustaría%20saber%20mas%20de%20sus%20servicios"><i className="fab fa-whatsapp"> Start Now!!</i></a>
                         </div>
                     <div className="col-md-4 mb-3 text-white">
